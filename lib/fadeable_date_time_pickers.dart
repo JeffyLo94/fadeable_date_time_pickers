21671d3bbd1642f28ccbbbc84787c95e3803342c
Jeffrey Lo <jeffrey.lo@axonics.com>
2022-11-17 11:08:19 -0800

    Removed hi_time_picker from exports

--- lib/fadeable_date_time_pickers.dart
+++ lib/fadeable_date_time_pickers.dart
@@ -4,6 +4,5 @@ export 'src/fadeable_horizontal_date_picker/fadeable_horizontal_date_picker.dart
 export 'src/fadeable_horizontal_date_picker/date_item_models.dart' show StateColor, StateFontWeight, DateItem, DateItemState;
 export 'src/fadeable_horizontal_date_picker/date_item_widget.dart' show DateItemWidget;
 export 'src/fadeable_spinner_time_picker/fadeable_spinner_time_picker.dart' show FadeableSpinnerTimePicker;
-export 'src/fadeable_spinner_time_picker/fadeable_hi_time_picker.dart' show FadeableTimePicker;
 export 'src/utils/date_time_picker_controller.dart' show DateTimePickerController;
 export 'src/utils/time_utils.dart';
